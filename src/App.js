21a8053a73bed608661a3adf678390b6adea3252
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-06 05:24:09 +0530

    🚀 template for practice

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+	return (
+		<main className="flex justify-center items-center h-screen">
+			<p className="text-2xl capitalize p-2 bg-red-300">
+				use this template for react practice with tailwind
+			</p>
+		</main>
+	);
+};
+
+export default App;
