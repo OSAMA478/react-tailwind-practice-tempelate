24a6bae1427a80e14e50fb12344a75b39be13d5c
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-07 00:17:59 +0530

    ✨ feat: added react icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { SiTailwindcss } from "react-icons/si";
+
 const App = () => {
 	return (
-		<main className="flex justify-center items-center h-screen">
-			<p className="text-2xl capitalize p-2 bg-red-300">
+		<main className="flex items-center justify-center h-screen">
+			<p className="p-2 text-2xl capitalize bg-red-300">
 				use this template for react practice with tailwind
+				<SiTailwindcss className="inline mx-1 text-cyan-500" />
 			</p>
 		</main>
 	);
